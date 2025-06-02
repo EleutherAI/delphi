84a01a4e08dedd8e61bf9de72e6e399e2d64d824
SrGonao <goncalo@eleuther.ai>
2025-06-02 15:10:05 +0000

    Add back all tests

--- tests/test_latents/test_constructor.py
+++ tests/test_latents/test_constructor.py
@@ -28,103 +28,103 @@ def test_save_load_cache(
         tokenizer,
     )
     tokens: Int[Tensor, "examples ctx_len"] = dataset.load_tokens()  # type: ignore
-    # assert (tokens == cache_setup["tokens"][: len(tokens)]).all()
+    assert (tokens == cache_setup["tokens"][: len(tokens)]).all()
     print(tokens.shape)
     for record in dataset:
-        print(record)
-    #     assert len(record.train) <= sampler_cfg.n_examples_train
-    #     assert len(record.test) <= sampler_cfg.n_examples_test
+        print(len(record.train), len(record.test))
+        assert len(record.train) <= sampler_cfg.n_examples_train
+        assert len(record.test) <= sampler_cfg.n_examples_test
 
 
-# @pytest.fixture(scope="module")
-# def seed():
-#     random.seed(0)
-#     torch.manual_seed(0)
+@pytest.fixture(scope="module")
+def seed():
+    random.seed(0)
+    torch.manual_seed(0)
 
 
-# @pytest.mark.parametrize("n_samples", [5, 10, 100, 1000])
-# @pytest.mark.parametrize("n_quantiles", [2, 5, 10, 23])
-# @pytest.mark.parametrize("n_examples", [0, 2, 5, 10, 20])
-# @pytest.mark.parametrize("train_type", ["top", "random", "quantiles"])
-# def test_simple_cache(
-#     n_samples: int,
-#     n_quantiles: int,
-#     n_examples: int,
-#     train_type: Literal["top", "random", "quantiles"],
-#     ctx_len: int = 32,
-#     seed: None = None,
-#     *,
-#     tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast,
-# ):
-#     torch.manual_seed(0)
-#     tokens = torch.randint(
-#         0,
-#         100,
-#         (
-#             n_samples,
-#             ctx_len,
-#         ),
-#     )
-#     all_activation_data = []
-#     all_activations = []
-#     for feature_idx in range(2):
-#         activations = torch.rand(n_samples, ctx_len, 1) * (
-#             torch.rand(n_samples)[..., None, None] ** 2
-#         )
-#         all_activations.append(activations)
-#         mask = activations > 0.1
-#         locations = torch.nonzero(mask)
-#         locations[..., 2] = feature_idx
-#         all_activation_data.append(ActivationData(locations, activations[mask]))
-#     activation_data, other_activation_data = all_activation_data
-#     activations, other_activations = all_activations
-#     record = LatentRecord(latent=Latent("test", 0), examples=[])
-#     constructor(
-#         record,
-#         activation_data,
-#         constructor_cfg=ConstructorConfig(
-#             example_ctx_len=ctx_len,
-#             min_examples=1,
-#             n_non_activating=50,
-#             non_activating_source="neighbours",
-#         ),
-#         tokens=tokens,
-#         tokenizer=tokenizer,
-#         all_data={0: activation_data, 1: other_activation_data},
-#     )
-#     for i, j in zip(record.examples[:-1], record.examples[1:]):
-#         assert i.max_activation >= j.max_activation
-#     for i in record.examples:
-#         index = (tokens == i.tokens).all(dim=-1).float().argmax()
-#         assert (tokens[index] == i.tokens).all()
-#         assert activations[index].max() == i.max_activation
-#     sampler(
-#         record,
-#         SamplerConfig(
-#             n_examples_train=n_examples,
-#             n_examples_test=n_examples,
-#             n_quantiles=n_quantiles,
-#             train_type=train_type,
-#             test_type="quantiles",
-#         ),
-#         tokenizer=tokenizer,
-#     )
-#     assert len(record.train) <= n_examples
-#     assert len(record.test) <= n_examples
-#     for neighbor in record.neighbours:
-#         assert neighbor.latent_index == 1
-#     for example in chain(record.train, record.test):
-#         assert isinstance(example, ActivatingExample)
-#         assert example.normalized_activations is not None
-#         assert example.normalized_activations.shape == example.activations.shape
-#         assert (example.normalized_activations <= 10).all()
-#         assert (example.normalized_activations >= 0).all()
-#     for quantile_list in (record.test,) + (  # type: ignore
-#         (record.train,) if train_type == "quantiles" else ()
-#     ):
-#         quantile_list: list[ActivatingExample] = quantile_list
-#         for k, i in enumerate(quantile_list):
-#             for j in quantile_list[k + 1 :]:
-#                 if i.quantile != j.quantile:
-#                     assert i.max_activation >= j.max_activation
-#                     assert i.quantile < j.quantile
+@pytest.mark.parametrize("n_samples", [5, 10, 100, 1000])
+@pytest.mark.parametrize("n_quantiles", [2, 5, 10, 23])
+@pytest.mark.parametrize("n_examples", [0, 2, 5, 10, 20])
+@pytest.mark.parametrize("train_type", ["top", "random", "quantiles"])
+def test_simple_cache(
+    n_samples: int,
+    n_quantiles: int,
+    n_examples: int,
+    train_type: Literal["top", "random", "quantiles"],
+    ctx_len: int = 32,
+    seed: None = None,
+    *,
+    tokenizer: PreTrainedTokenizer | PreTrainedTokenizerFast,
+):
+    torch.manual_seed(0)
+    tokens = torch.randint(
+        0,
+        100,
+        (
+            n_samples,
+            ctx_len,
+        ),
+    )
+    all_activation_data = []
+    all_activations = []
+    for feature_idx in range(2):
+        activations = torch.rand(n_samples, ctx_len, 1) * (
+            torch.rand(n_samples)[..., None, None] ** 2
+        )
+        all_activations.append(activations)
+        mask = activations > 0.1
+        locations = torch.nonzero(mask)
+        locations[..., 2] = feature_idx
+        all_activation_data.append(ActivationData(locations, activations[mask]))
+    activation_data, other_activation_data = all_activation_data
+    activations, other_activations = all_activations
+    record = LatentRecord(latent=Latent("test", 0), examples=[])
+    constructor(
+        record,
+        activation_data,
+        constructor_cfg=ConstructorConfig(
+            example_ctx_len=ctx_len,
+            min_examples=1,
+            n_non_activating=50,
+            non_activating_source="neighbours",
+        ),
+        tokens=tokens,
+        tokenizer=tokenizer,
+        all_data={0: activation_data, 1: other_activation_data},
+    )
+    for i, j in zip(record.examples[:-1], record.examples[1:]):
+        assert i.max_activation >= j.max_activation
+    for i in record.examples:
+        index = (tokens == i.tokens).all(dim=-1).float().argmax()
+        assert (tokens[index] == i.tokens).all()
+        assert activations[index].max() == i.max_activation
+    sampler(
+        record,
+        SamplerConfig(
+            n_examples_train=n_examples,
+            n_examples_test=n_examples,
+            n_quantiles=n_quantiles,
+            train_type=train_type,
+            test_type="quantiles",
+        ),
+        tokenizer=tokenizer,
+    )
+    assert len(record.train) <= n_examples
+    assert len(record.test) <= n_examples
+    for neighbor in record.neighbours:
+        assert neighbor.latent_index == 1
+    for example in chain(record.train, record.test):
+        assert isinstance(example, ActivatingExample)
+        assert example.normalized_activations is not None
+        assert example.normalized_activations.shape == example.activations.shape
+        assert (example.normalized_activations <= 10).all()
+        assert (example.normalized_activations >= 0).all()
+    for quantile_list in (record.test,) + (  # type: ignore
+        (record.train,) if train_type == "quantiles" else ()
+    ):
+        quantile_list: list[ActivatingExample] = quantile_list
+        for k, i in enumerate(quantile_list):
+            for j in quantile_list[k + 1 :]:
+                if i.quantile != j.quantile:
+                    assert i.max_activation >= j.max_activation
+                    assert i.quantile < j.quantile
