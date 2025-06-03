cd43058b9f3c96a25e5c81494f8cebdc78cfc814
Md Mahbubur Rahman <mdrahman@iastate.edu>
2025-06-03 00:06:35 -0700

    Fix: sae input is collected instead of sae latent

--- delphi/latents/cache.py
+++ delphi/latents/cache.py
@@ -277,7 +277,7 @@ class LatentCache:
                         self.model(batch.to(self.model.device))
 
                         for hookpoint, latents in activations.items():
-                            sae_latents = self.hookpoint_to_sparse_encode[hookpoint](
+                            sae_latents = self.hookpoint_to_sparse_encode[hookpoint].encode(
                                 latents
                             )
                             self.cache.add(sae_latents, batch, batch_number, hookpoint)
